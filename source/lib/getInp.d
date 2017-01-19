2f6afc2ae8a02ed77ab6b2c8eaa47edc9ae19ca2
MggMuggins <mggmugginsmc@gmail.com>
2017-01-18 20:17:55 -0600

    Added a few Comments and some other debugging stuff for File Read

--- source/lib/getInp.d
+++ source/lib/getInp.d
@@ -5,8 +5,11 @@ import help;
 
 ulong[] getInp (bool help, bool file, bool inp, string[] args) {
 	ulong[] numbs;
+	string carry;
 	int i = 0;
 	File input;
+	
+	//Help and Errors...
 	if ((help == true && inp == true) || (help == true && file == true)) {
 		printHelp();
 		return numbs;
@@ -16,7 +19,10 @@ ulong[] getInp (bool help, bool file, bool inp, string[] args) {
 	} if (help == true) {
 		printHelp();
 		return numbs;
-	} if (inp == true && file == false) {
+	} 
+	
+	//Actual Read Methods...
+	if (inp == true && file == false) {
 		for(i = 1; i < args.length; ++i) {
 			numbs ~= to!ulong(args[i]);
 		}
@@ -24,10 +30,16 @@ ulong[] getInp (bool help, bool file, bool inp, string[] args) {
 		input = File(args[1], "r");
 		while (!input.eof()) {
 			i = 0;
-			numbs[i] = to!ulong(strip(file.readln()));
+			//This isn't working. It's hanging on line 35 and not continuing with execution, with only one condition check.
+			writeln(i);
+			//Carry is defined to split up the error for debugging. It will be removed before the release
+			carry = strip(file.readln());
+			
+			writeln(carry);
+			numbs ~= to!ulong(carry);
 			++i;
 		}
 	}
-	//writeln(numbs);
+	writeln(numbs);
 	return numbs;
 }
